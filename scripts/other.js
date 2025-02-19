8daada497696e9e8a0253fbd685c03c2742d7377
GingerEdition22 <185860158+GingerEdition22@users.noreply.github.com>
2025-02-18 21:49:34 -0500

    Initial Commit

--- scripts/other.js
+++ scripts/other.js
@@ -2,3 +2,10 @@ function changeThemeColor(){
     const pickerVal = document.getElementById("themeColorPicker").value;
     document.documentElement.style.setProperty("--themeColor",pickerVal);
 }
+function typeWriter(id){
+    let object = document.getElementById(id);
+    if(object.innerHTML.length<object.dataset.textValue.length){
+        object.innerHTML+=object.dataset.textValue.charAt(object.innerHTML.length);
+        setTimeout(()=>typeWriter(id),50);
+    }
+}
